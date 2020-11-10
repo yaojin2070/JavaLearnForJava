035fc32a06f4ce7f2e6113ab705b6ace134982bc
YaoJin <87442144@qq.com>
2020-11-10 21:07:51 +0800

    java高级线程初试

---
+++ src/com/yjdzm/java/ThreadTest.java
@@ -0,0 +1,26 @@
+package com.yjdzm.java;
+
+/**
+ * @author YaoJin
+ * @create 2020-11-10 20:55
+ */
+class MyThread extends Thread {
+    @Override
+    public void run () {
+        for (int i = 0; i < 100 ; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+public class ThreadTest {
+    public static void main(String[] args) {
+        // 这里还是主线程进行的, 创建了一个对象
+        MyThread thread = new MyThread();
+        // 这里才是创建了一个线程
+        thread.start();
+        System.out.println("Hello, YaoJin");
+    }
+}
